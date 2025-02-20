2677402d2e1ca32a3ff05189dbdb78e5b3d9caeb
Lalit Max <lalitmay@gmail.com>
2025-02-20 23:12:42 +0530

    some socket setup in backend and init in frontend

--- client/src/lib/store/store.js
+++ client/src/lib/store/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import socketReducer from '../features/socket/socketSlice';
-
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    socket: socketReducer,
     // Add other reducers here
   },
 });
