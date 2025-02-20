e3abdf32397b1b00220a535220ae34289adcadea
Lalit Max <lalitmay@gmail.com>
2025-02-20 23:02:19 +0530

    fixed merge confilict will come

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -10,7 +10,6 @@ export default function Page() {
   return (
     <div className=" min-h-screen bg-gradient-to-b from-blue-50 to-white pt-5">
       <Navbar />
-    
       <Hero />
     </div>
   )
